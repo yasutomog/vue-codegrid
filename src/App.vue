c2f5dd8e5a1c4601a20d122af19293dd6b62d588
後藤恭智 <yasutomo.goto@xenophy.com>
2017-04-30 22:21:43 +0900

    add vol4 sample

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,59 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <editor-view @add="add"></editor-view>
+    <list-view :memos="memos" @remove="remove"></list-view>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'app'
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '../static/styles.css';
 </style>
+<script>
+  import EditorView from './components/EditorView'
+  import ListView from './components/ListView'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        memos: [
+          {
+            id: 1,
+            text: 'テスト',
+            date: '16-10-28',
+            tags: ['タグ1', 'タグ2']
+          },
+          {
+            id: 2,
+            text: 'テスト2',
+            date: '16-11-28',
+            tags: ['タグ2', 'タグ3']
+          }
+        ]
+      }
+    },
+    computed: {
+      nextId () {
+        return this.memos.reduce(
+          (id, memo) => {
+            return id < memo.id ? memo.id : id
+          }, 0) + 1
+      }
+    },
+    methods: {
+      add (newMemo) {
+        newMemo.id = this.nextId
+        this.memos.push(newMemo)
+      },
+      remove (id) {
+        const index = this.memos.findIndex((memo) => {
+          return memo.id === id
+        })
+        this.memos.splice(index, 1)
+      }
+    },
+    components: {
+      'editor-view': EditorView,
+      'list-view': ListView
+    }
+  }
+</script>
